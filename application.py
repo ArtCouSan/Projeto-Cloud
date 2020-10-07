2bb506becc417a6a830f629d1972631cb677a84a
Arthur Coutinho Santos <arthurcoutinho1998@gmail.com>
2020-10-06 21:34:46 -0300

    te

--- application.py
+++ application.py
@@ -3,9 +3,10 @@ import boto3
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 dynamoTable = dynamodb.Table('images')
-app = Flask(__name__)
 
-@app.route('/')
+application = Flask(__name__)
+
+@application.route("/")
 def index():
     response = dynamoTable.scan()
     data = response['Items']
@@ -15,4 +16,4 @@ def index():
     return '<h1 style="text-align: center;">Arthur Coutinho Santos</h1><h1 style="text-align: center;">RM336256</h1><div style="display: flex;justify-content: center;"><img src="%s" /></div>' % (url)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    application.run(host='0.0.0.0',debug=True)
